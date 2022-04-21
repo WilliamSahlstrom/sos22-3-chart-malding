dff658bcb35dca2b2136bd4b9ff4c37be1b37189
William Sahlström <sahlstrw@arcada.fi>
2022-04-21 13:43:58 +0300

    mellan commit

--- app/src/main/java/fi/arcada/sos_projekt_chart_sma/MainActivity.java
+++ app/src/main/java/fi/arcada/sos_projekt_chart_sma/MainActivity.java
@@ -4,29 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    String currency, datefrom, dateto;
+    // deklarera variabler
     LineChart chart;
+    EditText textInput;
+    String currency, datefrom, dateto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        chart = (Linechart) findViewById(R.id.chart);
 
+        // Initialisera view-variabler
+        chart = (LineChart) findViewById(R.id.chart);
         // TEMPORÄRA VÄRDEN
         currency = "USD";
         datefrom = "2022-01-01";
@@ -34,19 +39,19 @@ public class MainActivity extends AppCompatActivity {
 
         // Hämta växelkurser från API
         ArrayList<Double> currencyValues = getCurrencyValues(currency, datefrom, dateto);
-        // Skriv ut dem i konsolen
-        System.out.println(currencyValues.toString());
-
 
         ArrayList<Double> temperatures = Statistics.getDataValues();
-        System.out.println(temperatures);
-        ArrayList<Double> tempSma = Statistics.sma(temperatures, 3);
-        System.out.println(tempSma);
+        ArrayList<Double> tempsSma = Statistics.sma(currencyValues, 3);
+        ArrayList<Double> tempsSma2 = Statistics.sma(currencyValues, 10);
 
-        createChart(temperatures);
+        ArrayList<ChartLine> chartLines = new ArrayList<>();
+        chartLines.add(new ChartLine(temperatures, "Temperatur", Color.BLUE, 0));
+        chartLines.add(new ChartLine(tempsSma, "SMA3", Color.GREEN, 3));
+        chartLines.add(new ChartLine(tempsSma2, "SMA10", Color.RED, 10));
+
+        createBetterMultilineGraph(chartLines);
+
     }
-
-
     // Färdig metod som hämtar växelkursdata
     public ArrayList<Double> getCurrencyValues(String currency, String from, String to) {
 
@@ -71,20 +76,80 @@ public class MainActivity extends AppCompatActivity {
 
         return currencyData;
     }
-    public void createChart(ArrayList<Double> dataSet) {
-        List<Entry> entries = new ArrayList<Entry>();
+    // Metod för att skapa testdata
+    public static ArrayList<Double> getDataValues(ArrayList<Double> currencyValues){
 
-        for (Double val: dataSet) {
-            for (int i = 0; i < ; i++) {
-                entries.add(new Entry(i, dataSet.get(i).floatValue()));
+
+        // Skapa ny arraylist för Double-värden
+        ArrayList<Double> dataValues = new ArrayList<>();
+        // Loopa igenom dataItems och spara endast värdena i den nya arrayListen
+        for (double temp: currencyValues) {
+            dataValues.add(temp);
+            System.out.println(dataValues);
+        }
+
+        return dataValues;
+
+    }
+
+
+
+    /**
+     * Bättre graf med riktiga klassobjekt som linjer!
+     *
+     * @param chartLines
+     */
+    public void createBetterMultilineGraph(ArrayList<ChartLine> chartLines) {
+        List<ILineDataSet> dataSeries = new ArrayList<>();
+
+        for (ChartLine chartLine: chartLines) {
+            LineDataSet lineDataSet = new LineDataSet(chartLine.getEntries(), chartLine.getLabel());
+
+            lineDataSet.setColor(chartLine.getColor());
+            lineDataSet.setDrawCircles(false);
+            lineDataSet.setDrawValues(false);
+            dataSeries.add(lineDataSet);
+        }
+
+        LineData lineData = new LineData(dataSeries);
+        chart.setData(lineData);
+        chart.invalidate(); // refresh
+
+    }
+
+
+    public void createMultilineGraph(ArrayList<ArrayList<Double>> dataSets) {
+        List<ILineDataSet> dataSeries = new ArrayList<>();
+        List<Entry> entries;
+        //int entryOffset =  0; // var på X ska linjen börja
+
+        String[] labels = { "Temperatur", "SMA", "SMA2", "SMA3" };
+        int[] colors = { Color.BLUE, Color.RED, Color.GREEN, Color.BLACK };
+
+        LineDataSet lineDataSet;
+
+        for (int i = 0; i < dataSets.size(); i++) {
+            // Det dataset vi nu loopar
+            ArrayList<Double> currentDataSet = dataSets.get(i);
+            int entryOffset = dataSets.get(0).size() - currentDataSet.size();
+
+            entries = new ArrayList<Entry>();
+            for (int j = 0; j < currentDataSet.size(); j++) {
+                // OBS i = yttre loopen, j = inre loopen
+                entries.add(new Entry(j+entryOffset, currentDataSet.get(j).floatValue()));
             }
 
-            LineDataSet lineDataSet = new LineDataSet(entries, "Temperatur");
-            lineData lineData = new lineData(lineDataSet);
-
-            chart.setData(lineData);
-            chart.invalidate();
+            lineDataSet = new LineDataSet(entries, labels[i]);
+            lineDataSet.setColor(colors[i]);
+            lineDataSet.setDrawCircles(false);
+            lineDataSet.setDrawValues(false);
+            dataSeries.add(lineDataSet);
         }
+
+        LineData lineData = new LineData(dataSeries);
+        chart.setData(lineData);
+        chart.invalidate(); // refresh
     }
+
 
 }
--- app/src/main/java/fi/arcada/sos_projekt_chart_sma/Statistics.java
+++ app/src/main/java/fi/arcada/sos_projekt_chart_sma/Statistics.java
@@ -1,30 +1,32 @@
 package fi.arcada.sos_projekt_chart_sma;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Random;
 
 public class Statistics {
 
-    public static ArrayList<Double> sma(ArrayList<Double> dataset, int window){
+    public static ArrayList<Double> sma(ArrayList<Double> dataset, int window) {
         ArrayList<Double> ma = new ArrayList<>();
 
-        for (int i = window-1; i <dataset.size(); i++) {
-
+        for (int i = window-1; i < dataset.size(); i++) {
             double sum = 0.0;
 
             for (int j = 0; j < window; j++) {
-                sum += dataset.get(i - j);
+                sum += dataset.get(i - j); // t.ex. i:2 blir 2-0, 2-1, 2-2
             }
-
-            ma.add(sum / window);
+            ma.add(sum / window); // Medelvärde
         }
-
         return ma;
     }
-    // Metod för att skapa skild ArrayList med endast värdena från DataItems
 
+    // Metod för att skapa testdata
     public static ArrayList<Double> getDataValues() {
 
-        double[] temps = { -4.7, -4.8, -1.8, 0.7, 0.1, -6, -7.8, -7, -3.8, -10.6, -10.3, -0.3, 4.8, 2.6, 0.1, 1.2, -1.5, -2.7, 1.8, 0.2, -2, -5.5, -1.3, 2.1, -0.6, -0.9, 1, -0.5, -1.4, -1.6, -5.3, -7.7, -8.2, -9.5, -3.9, -0.4, 1, 0.8, -0.4, 0.6, 1, -1.5, -0.5, 1.4, 1.5, 1.8, 2, 1.1, -0.1, 0.1, -0.7, -0.4, -3, -6.8, 2, 1.5 };
+        Double[] temps = { -4.7, -4.8, -1.8,  0.7,  0.1, -6.0, -7.8, -7.0, -3.8, -10.6, -10.3, -0.3, 4.8, 2.6,  0.1, 1.2, -1.5, -2.7,  1.8,  0.2, -2.0, -5.5, -1.3,  2.1, -0.6, -0.9,  1.0, -0.5, -1.4, -1.6, -5.3 };
+        //Double[] sma =   {                   -2.0, -0.3, -1.7, -4.6, -6.9, -6.2, -7.1, -8.2, -7.1, -1.9,  2.4,  2.5, 1.3, -0.1, -1.0, -0.8, -0.2,  0.0, -2.4, -2.9, -1.6,  0.1,  0.2, -0.2, -0.1, -0.3, -1.2, -2.8 };
+
 
         // Skapa ny arraylist för Double-värden
         ArrayList<Double> dataValues = new ArrayList<>();
@@ -34,4 +36,7 @@ public class Statistics {
         }
         return dataValues;
     }
+
+
+
 }
