158a74916b13bb99b0352a08e7628a61827c4129
William Sahlström <sahlstrw@arcada.fi>
2022-04-22 14:15:03 +0300

    INLÄMNING

--- app/src/main/java/fi/arcada/sos_projekt_chart_sma/MainActivity.java
+++ app/src/main/java/fi/arcada/sos_projekt_chart_sma/MainActivity.java
@@ -3,11 +3,15 @@ package fi.arcada.sos_projekt_chart_sma;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.preference.PreferenceManager;
 
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.data.Entry;
@@ -25,34 +29,162 @@ public class MainActivity extends AppCompatActivity {
     LineChart chart;
     EditText textInput;
     String currency, datefrom, dateto;
+    TextView dFrom, dTo, textView;
+    Button button, button2;
+    Boolean test2 = false;
+    Boolean test3 = false;
+
+
+    //Objekt för preferences
+    SharedPreferences sharedPref;
+    SharedPreferences.Editor prefEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
         // Initialisera view-variabler
+        button = findViewById(R.id.button);
+        button2 = findViewById(R.id.button2);
+        textView = findViewById(R.id.textView);
+        dFrom = findViewById(R.id.dFrom);
+        dTo = findViewById(R.id.dTo);
         chart = (LineChart) findViewById(R.id.chart);
-        // TEMPORÄRA VÄRDEN
-        currency = "USD";
-        datefrom = "2022-01-01";
-        dateto = "2022-02-01";
+        //Ta in currency
+        currency = sharedPref.getString("currencyChange", "USD");
+        datefrom = sharedPref.getString("dateFrom", "2022-02-01");
+        dateto = sharedPref.getString("dateTo", "2022-03-01");
+
+
 
         // Hämta växelkurser från API
         ArrayList<Double> currencyValues = getCurrencyValues(currency, datefrom, dateto);
 
-        ArrayList<Double> temperatures = Statistics.getDataValues();
-        ArrayList<Double> tempsSma = Statistics.sma(currencyValues, 3);
-        ArrayList<Double> tempsSma2 = Statistics.sma(currencyValues, 10);
+        //Datum
+        dFrom.setText(String.format("%s",sharedPref.getString("dateFrom", null)));
+        dTo.setText(String.format("%s",sharedPref.getString("dateTo", null)));
+        textView.setText(String.format("%s", sharedPref.getString("currencyChange", "USD")));
+
+        String customSma = sharedPref.getString("sma1", "0");
+        int number = Integer.parseInt(customSma);
+        System.out.println(number);
+
+
+
+        ArrayList<Double> temperatures = currencyValues;
+        //ArrayList<Double> tempsSma = Statistics.sma(currencyValues, 3);
+        //ArrayList<Double> tempsSma2 = Statistics.sma(currencyValues, 10);
+
+        ArrayList<Double> tempsSma3 = Statistics.sma(currencyValues, number);
 
         ArrayList<ChartLine> chartLines = new ArrayList<>();
-        chartLines.add(new ChartLine(temperatures, "Temperatur", Color.BLUE, 0));
-        chartLines.add(new ChartLine(tempsSma, "SMA3", Color.GREEN, 3));
-        chartLines.add(new ChartLine(tempsSma2, "SMA10", Color.RED, 10));
+        chartLines.add(new ChartLine(temperatures, sharedPref.getString("currencyChange", "USD"), Color.BLUE, 0));
+        //chartLines.add(new ChartLine(tempsSma, "SMA3", Color.GREEN, 3));
+        //chartLines.add(new ChartLine(tempsSma2, "SMA10", Color.RED, 10));
+        if (number >0) {
+            chartLines.add(new ChartLine(tempsSma3, "SMACustom", Color.MAGENTA, number));
+        }
 
         createBetterMultilineGraph(chartLines);
 
     }
+    public void buttonClick(View view) {
+        Button btn = (Button) view;
+        if (view.getId() == R.id.button) {
+            if (test2 == false) {
+                test2 = true;
+                // Hämta växelkurser från API
+                ArrayList<Double> currencyValues = getCurrencyValues(currency, datefrom, dateto);
+                ArrayList<Double> temperatures = currencyValues;
+                ArrayList<Double> tempsSma = Statistics.sma(currencyValues, 3);
+                ArrayList<ChartLine> chartLines = new ArrayList<>();
+                chartLines.add(new ChartLine(temperatures, sharedPref.getString("currencyChange", "USD"), Color.BLUE, 0));
+                chartLines.add(new ChartLine(tempsSma, "SMA3", Color.GREEN, 3));
+                String customSma = sharedPref.getString("sma1", "0");
+                int number = Integer.parseInt(customSma);
+                ArrayList<Double> tempsSma3 = Statistics.sma(currencyValues, number);
+                if (number >0) {
+                    chartLines.add(new ChartLine(tempsSma3, "SMACustom", Color.MAGENTA, number));
+                }
+                if (test3 == true) {
+                    // Hämta växelkurser från API
+                    ArrayList<Double> tempsSma2 = Statistics.sma(currencyValues, 10);
+                    chartLines.add(new ChartLine(tempsSma2, "SMA10", Color.RED, 10));
+                }
+                createBetterMultilineGraph(chartLines);
+            } else if (test2 == true) {
+                // Hämta växelkurser från API
+                ArrayList<Double> currencyValues = getCurrencyValues(currency, datefrom, dateto);
+                ArrayList<Double> temperatures = currencyValues;
+                ArrayList<ChartLine> chartLines = new ArrayList<>();
+                chartLines.add(new ChartLine(temperatures, sharedPref.getString("currencyChange", "USD"), Color.BLUE, 0));
+                String customSma = sharedPref.getString("sma1", "0");
+                int number = Integer.parseInt(customSma);
+                ArrayList<Double> tempsSma3 = Statistics.sma(currencyValues, number);
+                if (number >0) {
+                    chartLines.add(new ChartLine(tempsSma3, "SMACustom", Color.MAGENTA, number));
+                }
+                if (test3 == true) {
+                    // Hämta växelkurser från API
+                    ArrayList<Double> tempsSma2 = Statistics.sma(currencyValues, 10);
+                    chartLines.add(new ChartLine(tempsSma2, "SMA10", Color.RED, 10));
+                }
+                createBetterMultilineGraph(chartLines);
+                test2 = false;
+            }
+
+        }
+        if (view.getId() == R.id.button2) {
+            if (test3 == false) {
+                test3 = true;
+                // Hämta växelkurser från API
+                ArrayList<Double> currencyValues = getCurrencyValues(currency, datefrom, dateto);
+                ArrayList<Double> temperatures = currencyValues;
+                ArrayList<Double> tempsSma2 = Statistics.sma(currencyValues, 10);
+                ArrayList<ChartLine> chartLines = new ArrayList<>();
+                chartLines.add(new ChartLine(temperatures, sharedPref.getString("currencyChange", "USD"), Color.BLUE, 0));
+                chartLines.add(new ChartLine(tempsSma2, "SMA10", Color.RED, 10));
+                String customSma = sharedPref.getString("sma1", "0");
+                int number = Integer.parseInt(customSma);
+                ArrayList<Double> tempsSma3 = Statistics.sma(currencyValues, number);
+                if (number >0) {
+                    chartLines.add(new ChartLine(tempsSma3, "SMACustom", Color.MAGENTA, number));
+                }
+                if (test2 == true) {
+                    // Hämta växelkurser från API
+                    ArrayList<Double> tempsSma = Statistics.sma(currencyValues, 3);
+                    chartLines.add(new ChartLine(tempsSma, "SMA3", Color.GREEN, 3));
+                }
+                createBetterMultilineGraph(chartLines);
+
+            }
+            //Default view i SMA3 knappen
+            else if (test3 == true) {
+
+                // Hämta växelkurser från API
+                ArrayList<Double> currencyValues = getCurrencyValues(currency, datefrom, dateto);
+                ArrayList<Double> temperatures = currencyValues;
+                ArrayList<ChartLine> chartLines = new ArrayList<>();
+                chartLines.add(new ChartLine(temperatures, sharedPref.getString("currencyChange", "USD"), Color.BLUE, 0));
+                String customSma = sharedPref.getString("sma1", "0");
+                int number = Integer.parseInt(customSma);
+                ArrayList<Double> tempsSma3 = Statistics.sma(currencyValues, number);
+                if (number >0) {
+                    chartLines.add(new ChartLine(tempsSma3, "SMACustom", Color.MAGENTA, number));
+                }
+                if (test2 == true) {
+                    ArrayList<Double> tempsSma = Statistics.sma(currencyValues, 3);
+                    chartLines.add(new ChartLine(tempsSma, "SMA3", Color.GREEN, 3));
+                }
+                createBetterMultilineGraph(chartLines);
+                test3 = false;
+            }
+
+        }
+    }
+
     // Färdig metod som hämtar växelkursdata
     public ArrayList<Double> getCurrencyValues(String currency, String from, String to) {
 
